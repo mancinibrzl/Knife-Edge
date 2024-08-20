acd3ba56f70ec61927b97837462b7a3a67e617bc
mancinibrzl <mariomancinineto2017@gmail.com>
2024-08-19 23:06:56 -0300

    create a target in the scene, and create his script, now the targer rotate in random directions with random speeds

---
+++ Knife Edge/Assets/Scripts/Target/Target.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [Header("Rotação do Alvo")]
+    [SerializeField] private int[] velocidadesDeRotacao;
+    private int velocidadeDeRotacaoAtual;
+
+    [Header("Cronômetro da Troca de Velocidade")]
+    [SerializeField] private float tempoMaximoParaTrocarVelocidade = 2f;
+    private float tempoAtualParaTrocarVelocidade;
+
+    private void Update()
+    {
+        RodarCronometroDaTrocaDeVelocidade();
+        RotacionarAlvo();
+    }
+
+    private void RodarCronometroDaTrocaDeVelocidade()
+    {
+        tempoAtualParaTrocarVelocidade -= Time.deltaTime;
+        if (tempoAtualParaTrocarVelocidade <= 0)
+        {
+            EscolherNovaVelocidadeDeRotacao();
+            tempoAtualParaTrocarVelocidade = tempoMaximoParaTrocarVelocidade;
+        }
+    }
+
+    private void EscolherNovaVelocidadeDeRotacao()
+    {
+        velocidadeDeRotacaoAtual = velocidadesDeRotacao[Random.Range(0, velocidadesDeRotacao.Length)];
+    }
+
+    private void RotacionarAlvo()
+    {
+        transform.Rotate(0f, 0f, velocidadeDeRotacaoAtual * Time.deltaTime);
+    }
+}
